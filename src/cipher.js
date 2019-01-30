e3b2fa2a61e5ac80ee73127aa53cdc4ddd887d2f
Belen-Alejandra <alejandra_ponce_89@hotmail.com>
2019-01-30 11:09:01 -0800

    Ingrese funciones code y deCode

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,24 @@
 window.cipher = {
-  // ... 
+  function alpha(string,desp){
+let salida= '';
+for (let i = 0; i < string.length; i++){
+   oldAscII = string[i].charCodeAt();
+    newAscII = (oldAscII - 65 + desp) % 26 + 65;
+    salida = salida.concat(String.fromCharCode(newAscII))
+    //console.log(String.fromCharCode(newAscII))
+}
+  document.write(salida)
+}
+
+function alphaDecod(string,desp){
+  let salida = '';
+  for (let i = 0; i<string.length;i++){
+     oldAscII = string[i].charCodeAt();
+
+      newAscII = (oldAscII + 65 - desp) % 26 + 65;
+      salida = salida.concat(String.fromCharCode(newAscII))
+      //salida=salida.concat(oldAscII[i]);
+  }
+  document.write(salida)
+}
 };
