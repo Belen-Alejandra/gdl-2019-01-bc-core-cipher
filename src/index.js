b90749b967252fa0cd6728a6592ea8aaa684603e
Belen-Alejandra <alejandra_ponce_89@hotmail.com>
2019-02-05 08:34:08 -0800

    llamado correcto de botones en index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 let cifrar =() => {
   const string = document.getElementById("text1").value;
   const offset = parseInt(document.getElementById("offset").value);
-  document.getElementById('enco').innerHTML = encode(offset,string);
+  document.getElementById('enco').innerHTML = window.cipher.encode(offset,string);
 
 };
 
 let descifrar =() => {
   const string = document.getElementById("text1").value;
   const offset = parseInt(document.getElementById("offset").value);
-  document.getElementById('enco').innerHTML = decode(offset,string);
+  document.getElementById('enco').innerHTML =window.cipher.decode(offset,string);
 
 };
+
+
+document.getElementById("encode").addEventListener('click', cifrar);
+document.getElementById("decode").addEventListener('click', descifrar);
